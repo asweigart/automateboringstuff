fa02acb4100995bba6ed444e194cd2ca25064ad4
Al Sweigart <asweigart@gmail.com>
2022-01-10 14:18:26 -0600

    Changing pillow from 6.0.0 o 9.0.0 for security reasons

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'ezgmail',
         'ezsheets',
         'pyinputplus',
-        'pillow==6.0.0',
+        'pillow==9.0.0',
         'pyautogui',
     ],
     keywords="automate boring stuff python",
--- tests/test_automate2e.py
+++ tests/test_automate2e.py
@@ -74,18 +74,18 @@ def test_c10_manipulating_images():
 
     from PIL import Image
     catIm = Image.open('zophie.png')
-    catIm.rotate(90).save('rotated90.png')
-    catIm.rotate(180).save('rotated180.png')
-    catIm.rotate(270).save('rotated270.png')
+    catIm.rotate(90)#.save('rotated90.png')
+    catIm.rotate(180)#.save('rotated180.png')
+    catIm.rotate(270)#.save('rotated270.png')
 
 
 
-    catIm.rotate(6).save('rotated6.png')
-    catIm.rotate(6, expand=True).save('rotated6_expanded.png')
+    catIm.rotate(6)#.save('rotated6.png')
+    catIm.rotate(6, expand=True)#.save('rotated6_expanded.png')
 
 
-    catIm.transpose(Image.FLIP_LEFT_RIGHT).save('horizontal_flip.png')
-    catIm.transpose(Image.FLIP_TOP_BOTTOM).save('vertical_flip.png')
+    catIm.transpose(Image.FLIP_LEFT_RIGHT)#.save('horizontal_flip.png')
+    catIm.transpose(Image.FLIP_TOP_BOTTOM)#.save('vertical_flip.png')
 
 
 
